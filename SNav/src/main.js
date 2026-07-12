adb675998e0bd257a77a40beffa518d042cd4ce5
Long <38917505+loongworld@users.noreply.github.com>
2026-07-13 03:48:02 +0800

    feat: 密码门禁(258888)+自动保存+折叠收藏+github.js修复

--- SNav/src/main.js
+++ SNav/src/main.js
@@ -6,6 +6,7 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import SvgIcon from "@/components/SvgIcon.vue";
 // 主组件
 import App from "@/App.vue";
+import { siteStore } from "@/stores";
 // 全局样式
 import "@/style/global.scss";
 
@@ -19,4 +20,11 @@ pinia.use(piniaPluginPersistedstate);
 // 挂载
 app.use(pinia);
 app.component("SvgIcon", SvgIcon);
+
+// 全局自动保存：siteData 任何改动 debounce 写 GitHub（用户零操作）
+const siteData = siteStore();
+siteData.$subscribe(() => {
+  siteData.scheduleSave();
+});
+
 app.mount("#app");
